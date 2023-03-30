b548d927d0b8a39fae9e2c8189d52f3f825c9c37
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2023-03-30 23:14:45 +0500

    server index.js requirement added

---
+++ server/index.js
@@ -0,0 +1,18 @@
+// Requirements:
+//1. SignUp api
+//2. Login api
+//3. Edit profile api
+//4. Logout profile api
+
+// usermodels:
+// email
+// phone no
+// username
+// password
+
+// Hashmodel
+// hash
+// privateKey
+
+// 1. create an api in which hash and private key is stored
+// 2. getPrivateKey api
