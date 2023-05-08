018722f617484a3b9fcb971a88a60f06d881585e
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2023-05-08 23:50:39 +0500

    contract changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -48,4 +48,7 @@ export default App;
 // 9012393383baacbaae91b9c349f47c1850c9b5d2745051f10f8202a4c8be033d
 
 // Ibrahim
-// mongodb+srv://Ibrahim:<password>@cluster0.b0htjfc.mongodb.net/test
+// mongodb+srv://Ibrahim:<Ibrahim>@cluster0.b0htjfc.mongodb.net/test
+
+// Ibrahim Almutairi
+// Naif Alanazi
